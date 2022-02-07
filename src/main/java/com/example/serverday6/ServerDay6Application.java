eb864d491fb81ffefe332099221b61d4ba6f44b8
Zhdzd <zahidabdullah18@gmail.com>
2022-02-07 22:44:05 +0800

    'commit'

---
+++ src/main/java/com/example/serverday6/ServerDay6Application.java
@@ -0,0 +1,13 @@
+package com.example.serverday6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerDay6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerDay6Application.class, args);
+	}
+
+}
